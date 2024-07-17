a1f79852a77df5ea4f38163834c6d3d3dfa2106f
Jack Lingle <captainjack0818@gmail.com>
2024-07-17 18:08:14 -0400

    finish the load_data method

--- traffic.py
+++ traffic.py
@@ -19,29 +19,31 @@ def main():
     if len(sys.argv) not in [2, 3]:
         sys.exit("Usage: python traffic.py data_directory [model.h5]")
 
-    # Get image arrays and labels for all image files
-    images, labels = load_data(sys.argv[1])
+    print(load_data(sys.argv[1]))
 
-    # Split data into training and testing sets
-    labels = tf.keras.utils.to_categorical(labels)
-    x_train, x_test, y_train, y_test = train_test_split(
-        np.array(images), np.array(labels), test_size=TEST_SIZE
-    )
-
-    # Get a compiled neural network
-    model = get_model()
-
-    # Fit model on training data
-    model.fit(x_train, y_train, epochs=EPOCHS)
-
-    # Evaluate neural network performance
-    model.evaluate(x_test,  y_test, verbose=2)
-
-    # Save model to file
-    if len(sys.argv) == 3:
-        filename = sys.argv[2]
-        model.save(filename)
-        print(f"Model saved to {filename}.")
+    # # Get image arrays and labels for all image files
+    # images, labels = load_data(sys.argv[1])
+    #
+    # # Split data into training and testing sets
+    # labels = tf.keras.utils.to_categorical(labels)
+    # x_train, x_test, y_train, y_test = train_test_split(
+    #     np.array(images), np.array(labels), test_size=TEST_SIZE
+    # )
+    #
+    # # Get a compiled neural network
+    # model = get_model()
+    #
+    # # Fit model on training data
+    # model.fit(x_train, y_train, epochs=EPOCHS)
+    #
+    # # Evaluate neural network performance
+    # model.evaluate(x_test,  y_test, verbose=2)
+    #
+    # # Save model to file
+    # if len(sys.argv) == 3:
+    #     filename = sys.argv[2]
+    #     model.save(filename)
+    #     print(f"Model saved to {filename}.")
 
 
 def load_data(data_dir):
@@ -58,7 +60,22 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    image_list = []
+    label_list = []
+
+    categories = os.listdir(data_dir)
+    for category in categories:
+        images = os.listdir(os.path.join(data_dir, category))
+        for image in images:
+            img = cv2.imread(os.path.join(data_dir, category, image))
+            resized_img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+            # normalize values to range of 0 - 1
+            resized_img = resized_img / 255.0
+            image_list.append(resized_img)
+            label_list.append(category)
+
+    return image_list, label_list
+
 
 
 def get_model():
